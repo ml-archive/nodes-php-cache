834f465250003c08bc3a3dbb737a8a07be6bbe41
Morten Rugaard <moru@nodes.dk>
2016-02-06 18:00:07 +0100

    Implemented install script

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,34 +1,61 @@
 <?php
 namespace Nodes\Cache;
 
-use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
-use Nodes\Cache\Console\Commands\FlushCache;
+use Nodes\AbstractServiceProvider;
 
 /**
  * Class ServiceProvider
- * @author  Casper Rasmussen <cr@nodes.dk>
  *
  * @package Nodes\Cache
  */
-class ServiceProvider extends IlluminateServiceProvider
+class ServiceProvider extends AbstractServiceProvider
 {
     /**
-     * register
+     * Package name
+     *
+     * @var string
+     */
+    protected $package = 'cache';
+
+    /**
+     * Facades to install
+     *
+     * @var array
+     */
+    protected $facades = [
+        'NodesCache' => \Nodes\Cache\Support\Facades\Cache::class
+    ];
+
+    /**
+     * Artisan commands to register
+     *
+     * @var array
+     */
+    protected $commands = [
+        \Nodes\Cache\Console\Commands\FlushCache::class
+    ];
+
+    /**
+     * Array of configs to copy
+     *
+     * @var array
+     */
+    protected $configs = [
+        'config/cache.php' => 'config/nodes/cache.php'
+    ];
+
+    /**
+     * Register service provider
      *
      * @author Casper Rasmussen <cr@nodes.dk>
      *
+     * @access public
      * @return void
      */
     public function register()
     {
+        parent::register();
         $this->registerCacheRepository();
-        $this->app['commands.nodes.cache.flush'] = $this->app->share(
-            function ($app) {
-                return new FlushCache();
-            }
-        );
-
-        $this->commands('commands.nodes.cache.flush');
     }
 
     /**
@@ -36,6 +63,7 @@ class ServiceProvider extends IlluminateServiceProvider
      *
      * @author Casper Rasmussen <cr@nodes.dk>
      *
+     * @access protected
      * @return void
      */
     protected function registerCacheRepository()
